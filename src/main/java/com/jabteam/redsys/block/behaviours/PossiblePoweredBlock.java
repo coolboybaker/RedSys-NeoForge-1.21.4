a23ab669ba8d27a06b93077be6811dea9709af7e
A`Sun <a.sunmain0@gmail.com>
2025-06-11 01:57:55 +0300

    block-update2

--- src/main/java/com/jabteam/redsys/block/behaviours/PossiblePoweredBlock.java
+++ src/main/java/com/jabteam/redsys/block/behaviours/PossiblePoweredBlock.java
@@ -1,18 +1,19 @@
 package com.jabteam.redsys.block.behaviours;
 
+
+import com.jabteam.redsys.block.ModBlocks;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.redstone.Orientation;
+import org.jetbrains.annotations.Nullable;
+
 
 public class PossiblePoweredBlock extends Block {
     public PossiblePoweredBlock(Properties properties) {
         super(properties);
     }
-
-
 }
