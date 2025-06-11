5e0734e274a258f7e0b127aa656451406fbfcc75
nixaty <nixaty@nixaty.ru>
2025-06-12 00:57:37 +0400

    Power state for aurium_block (not work)

--- src/main/java/com/jabteam/redsys/block/behaviours/PossiblePoweredBlock.java
+++ src/main/java/com/jabteam/redsys/block/behaviours/PossiblePoweredBlock.java
@@ -1,12 +1,43 @@
 package com.jabteam.redsys.block.behaviours;
 
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.redstone.Orientation;
+import org.jetbrains.annotations.Nullable;
 
 
 public class PossiblePoweredBlock extends Block {
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
+
     public PossiblePoweredBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(POWERED, false));
     }
+
+    @Override
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, @Nullable Orientation orientation, boolean movedByPiston) {
+        boolean powered = level.hasNeighborSignal(pos);
+        if (powered != state.getValue(POWERED)) {
+            level.setBlock(pos, state.setValue(POWERED, powered), 3);
+        }
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        boolean isPowered = context.getLevel().hasNeighborSignal(context.getClickedPos());
+        return this.defaultBlockState().setValue(POWERED, isPowered);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(POWERED);
+    }
+
 }
